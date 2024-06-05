593cb2d327a8dcb69899e62be2e7dc8cccf0da6b
Akshit Mittal <116814372+AkshitMital@users.noreply.github.com>
2024-06-05 23:32:06 +0530

    Time: 4 ms (31.68%), Space: 9.1 MB (81.55%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,21 +1,3 @@
-// class Solution {
-// private:
-//     int solve(vector<int>& nums, int index, vector<int>& dp){
-//         if(index == 0) return nums[index];
-//         if(index < 0) return 0;
-//         if(dp[index] != -1) return dp[index];
-//         int pick = nums[index] + solve(nums, index - 2, dp);
-//         int not_pick = 0 + solve(nums, index - 1, dp);
-
-//         return dp[index] = max(pick, not_pick);
-//     }
-// public:
-//     int rob(vector<int>& nums) {
-//         vector<int> dp(nums.size(), -1);
-//         return solve(nums, nums.size()-1, dp);
-//     }
-// };
-
 class Solution {
 public:
     int rob(vector<int>& nums) {
