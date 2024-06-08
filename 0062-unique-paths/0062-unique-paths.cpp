ca1a4c3469cf0b3b5d7fffd5fbb182bec9ba8519
Akshit Mittal <116814372+AkshitMital@users.noreply.github.com>
2024-06-08 16:58:46 +0530

    Time: 0 ms (100%), Space: 7.7 MB (44.99%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,33 +1,38 @@
 class Solution {
-private:
-    int solve(int row, int col, vector<vector<int>>& dp){
-        if(row == 0 && col == 0){
-            return 1;
-        }
-        if(row < 0 || col < 0){
-            return 0;
-        }
+// private:
+//     int solve(int row, int col, vector<vector<int>>& dp){
+//         if(row == 0 && col == 0){
+//             return 1;
+//         }
+//         if(row < 0 || col < 0){
+//             return 0;
+//         }
 
-        if(dp[row][col] != -1) return dp[row][col];
+//         if(dp[row][col] != -1) return dp[row][col];
 
-        int left = solve(row, col-1, dp);
-        int up = solve(row-1, col, dp);
+//         int left = solve(row, col-1, dp);
+//         int up = solve(row-1, col, dp);
 
-        return dp[row][col] = left + up;
-    }
+//         return dp[row][col] = left + up;
+//     }
 public:
-    int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n,-1));
-        return solve(m-1,n-1,dp);
-    }
     // int uniquePaths(int m, int n) {
     //     vector<vector<int>> dp(m, vector<int>(n,-1));
-    //     dp[0][0] = 1;
-
-    //     for(int i = 0; i < m; i++){
-    //         for(int j = 0; j < n; j++){
-    //             left = dp[]
-    //         }
-    //     }
+    //     return solve(m-1,n-1,dp);
     // }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n,0));
+        dp[0][0] = 1;
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(i == 0 && j == 0) continue;
+                int right = (j > 0) ? dp[i][j-1] : 0;
+                int down = (i > 0) ? dp[i-1][j] : 0;
+                dp[i][j] = right + down;
+            }
+        }
+
+        return dp[m-1][n-1];
+    }
 };
