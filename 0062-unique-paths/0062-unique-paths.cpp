b27a297004dc10d7578d35760b8be8547e9b41a7
Akshit Mittal <116814372+AkshitMital@users.noreply.github.com>
2024-06-08 16:58:54 +0530

    Time: 3 ms (32.98%), Space: 7.4 MB (71.39%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -20,19 +20,55 @@ public:
     //     vector<vector<int>> dp(m, vector<int>(n,-1));
     //     return solve(m-1,n-1,dp);
     // }
+    // int uniquePaths(int m, int n) {
+    //     vector<vector<int>> dp(m, vector<int>(n,0));
+    //     dp[0][0] = 1;
+
+    //     for(int i = 0; i < m; i++){
+    //         for(int j = 0; j < n; j++){
+    //             if(i == 0 && j == 0) continue;
+    //             int right = (j > 0) ? dp[i][j-1] : 0;
+    //             int down = (i > 0) ? dp[i-1][j] : 0;
+    //             dp[i][j] = right + down;
+    //         }
+    //     }
+
+    //     return dp[m-1][n-1];
+    // }
+
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n,0));
-        dp[0][0] = 1;
+        vector<int> prev(n,0);
 
         for(int i = 0; i < m; i++){
+            vector<int> temp(n,0);
             for(int j = 0; j < n; j++){
-                if(i == 0 && j == 0) continue;
-                int right = (j > 0) ? dp[i][j-1] : 0;
-                int down = (i > 0) ? dp[i-1][j] : 0;
-                dp[i][j] = right + down;
+                if(i == 0 && j == 0){
+                    temp[j] = 1;
+                }
+                else{
+                    int left = (j > 0) ? temp[j-1] : 0;
+                    int up = (i > 0) ? prev[j] : 0;
+                    temp[j] = up+left;
+                }
             }
+            prev = temp;
         }
 
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
+
+//             if (i == 0 && j == 0) {
+//                 temp[j] = 1;  // Set the starting point
+//             } else {
+//                 int left = (j > 0) ? temp[j-1] : 0;
+//                 int up = (i > 0) ? prev[j] : 0;
+//                 temp[j] = up + left;
+//             }
+//         }
+//         prev = temp;
+//     }
+
+//     return prev[n-1];
+// }
+
 };
