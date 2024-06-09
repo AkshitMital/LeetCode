1769188ddba1e5bd059e3a779c83691f459cbff4
Akshit Mittal <116814372+AkshitMital@users.noreply.github.com>
2024-06-09 17:37:14 +0530

    Time: 8 ms (27.04%), Space: 11.5 MB (10.19%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,21 +1,42 @@
+// class Solution {
+// private:
+//     int solve(int row, int col, vector<vector<int>>& triangle, vector<vector<int>>& dp){
+//         if(row == triangle.size() - 1){
+//             return triangle[row][col];
+//         }
+
+//         if(dp[row][col] != -1) return dp[row][col];
+
+//         int down = triangle[row][col] + solve(row+1, col, triangle, dp);
+//         int diagnol = triangle[row][col] + solve(row+1, col+1, triangle, dp);
+
+//         return dp[row][col] = min(diagnol, down);
+//     }
+// public:
+//     int minimumTotal(vector<vector<int>>& triangle) {
+//         int n = triangle.size();
+//         vector<vector<int>> dp(n, vector<int>(n,-1));
+//         return solve(0, 0, triangle, dp);
+//     }
+// };
+
 class Solution {
-private:
-    int solve(int row, int col, vector<vector<int>>& triangle, vector<vector<int>>& dp){
-        if(row == triangle.size()-1){
-            return triangle[row][col];
-        }
-
-        if(dp[row][col] != -1) return dp[row][col];
-
-        int down = triangle[row][col] + solve(row+1, col, triangle, dp);
-        int diagnol = triangle[row][col] + solve(row+1, col+1, triangle, dp);
-
-        return dp[row][col] = min(diagnol, down);
-    }
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<vector<int>> dp(n, vector<int>(n,-1));
-        return solve(0, 0, triangle, dp);
+        vector<vector<int>> dp(n, vector<int>(n,0));
+
+        for(int i = n-1; i >= 0; i--){
+            for(int j = i; j >= 0; j--){
+                if(i == n-1) dp[i][j] = triangle[i][j];
+                else{
+                    int down = triangle[i][j] + dp[i+1][j];
+                    int diagnol = triangle[i][j] + dp[i+1][j+1];
+                    dp[i][j] = min(diagnol, down);
+                }
+            }
+        }
+
+        return dp[0][0];
     }
 };
