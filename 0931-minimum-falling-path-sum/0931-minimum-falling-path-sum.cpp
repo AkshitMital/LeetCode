9fc6f7931b486432947a102e25487146f846d3ad
Akshit Mittal <116814372+AkshitMital@users.noreply.github.com>
2024-06-09 20:19:38 +0530

    Time: 16 ms (21.76%), Space: 13.1 MB (76.88%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -20,27 +20,55 @@
 //     }
 // };
 
+// class Solution {
+// public:
+//     int minFallingPathSum(vector<vector<int>>& matrix) {
+//         int n = matrix.size();
+//         vector<vector<int>> dp(n, vector<int>(n,0));
+//         for(int col = 0; col < n; col++){
+//             dp[0][col] = matrix[0][col]; 
+//         }
+
+//         for(int row = 1; row<n; row++){
+//             for(int col = 0; col< n; col++){
+//                 int up = matrix[row][col] + dp[row-1][col];
+//                 int left_diag = matrix[row][col] + ((col != 0) ? dp[row-1][col-1] : 1e9);
+//                 int right_diag = matrix[row][col] + ((col != matrix[0].size()-1) ? dp[row-1][col+1] : 1e9);
+//                 dp[row][col] = min({up, left_diag, right_diag});
+//             }
+//         }
+
+//         int minPathSum = INT_MAX;
+//         for(int col = 0; col < n; col++) {
+//             minPathSum = min(minPathSum, dp[n-1][col]);
+//         }
+//         return minPathSum;
+//     }
+// };
+
+
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        vector<vector<int>> dp(n, vector<int>(n,0));
+        vector<int> curr(n,0), prev(n,0);
         for(int col = 0; col < n; col++){
-            dp[0][col] = matrix[0][col]; 
+            prev[col] = matrix[0][col]; 
         }
 
         for(int row = 1; row<n; row++){
             for(int col = 0; col< n; col++){
-                int up = matrix[row][col] + dp[row-1][col];
-                int left_diag = matrix[row][col] + ((col != 0) ? dp[row-1][col-1] : 1e9);
-                int right_diag = matrix[row][col] + ((col != matrix[0].size()-1) ? dp[row-1][col+1] : 1e9);
-                dp[row][col] = min({up, left_diag, right_diag});
+                int up = matrix[row][col] + prev[col];
+                int left_diag = matrix[row][col] + ((col != 0) ? prev[col-1] : 1e9);
+                int right_diag = matrix[row][col] + ((col != matrix[0].size()-1) ? prev[col+1] : 1e9);
+                curr[col] = min({up, left_diag, right_diag});
             }
+            prev = curr;
         }
 
         int minPathSum = INT_MAX;
         for(int col = 0; col < n; col++) {
-            minPathSum = min(minPathSum, dp[n-1][col]);
+            minPathSum = min(minPathSum, prev[col]);
         }
         return minPathSum;
     }
