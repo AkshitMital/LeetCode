4da5e532425e2ea822d86c0431ee5d6dbc9f4463
Akshit Mittal <116814372+AkshitMital@users.noreply.github.com>
2024-06-09 23:44:46 +0530

    Time: 68 ms (30.16%), Space: 11.6 MB (95.1%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -35,17 +35,57 @@
 // };
 
 
+// class Solution {
+// public:
+//     int cherryPickup(vector<vector<int>>& grid) {
+//         int r = grid.size();
+//         int c = grid[0].size();
+//         vector<vector<vector<int>>> dp(r, vector<vector<int>>(c, vector<int>(c, 0)));
+
+//         for(int j1 = 0; j1 < c; j1++){
+//             for(int j2 = 0; j2 < c; j2++){
+//                 if(j1 == j2) dp[r-1][j1][j2] = grid[r-1][j1];
+//                 else dp[r-1][j1][j2] = grid[r-1][j1] + grid[r-1][j2];
+//             }
+//         }
+
+//         for(int i = r-2; i >= 0; i--){
+//             for(int j1 = 0; j1 < c; j1++){
+//                 for(int j2 = 0; j2 < c; j2++){
+//                     int maxi = -1e9;
+
+//                     for(int dj1 = -1; dj1 <= 1; dj1++){
+//                         for(int dj2 = -1; dj2 <= 1; dj2++){
+//                             int new_col1 = j1 + dj1;
+//                             int new_col2 = j2 + dj2;
+//                             int current_cherries = (j1 == j2) ? grid[i][j1] : grid[i][j1] + grid[i][j2];
+//                             if(new_col1 >= 0 && new_col1 < c && new_col2 >= 0 && new_col2 < c) current_cherries += dp[i+1][new_col1][new_col2];
+//                             else current_cherries += -1e9;
+//                             maxi = max(maxi, current_cherries);
+//                         }
+//                     }
+//                     dp[i][j1][j2] = maxi;
+//                 }
+//             }
+//         }
+        
+
+//         return dp[0][0][c-1];
+//     }
+// };
+
 class Solution {
 public:
     int cherryPickup(vector<vector<int>>& grid) {
         int r = grid.size();
         int c = grid[0].size();
-        vector<vector<vector<int>>> dp(r, vector<vector<int>>(c, vector<int>(c, 0)));
+        vector<vector<int>> front(c, vector<int>(c,0));
+        vector<vector<int>> curr(c, vector<int>(c,0));
 
         for(int j1 = 0; j1 < c; j1++){
             for(int j2 = 0; j2 < c; j2++){
-                if(j1 == j2) dp[r-1][j1][j2] = grid[r-1][j1];
-                else dp[r-1][j1][j2] = grid[r-1][j1] + grid[r-1][j2];
+                if(j1 == j2) front[j1][j2] = grid[r-1][j1];
+                else front[j1][j2] = grid[r-1][j1] + grid[r-1][j2];
             }
         }
 
@@ -59,17 +99,19 @@ public:
                             int new_col1 = j1 + dj1;
                             int new_col2 = j2 + dj2;
                             int current_cherries = (j1 == j2) ? grid[i][j1] : grid[i][j1] + grid[i][j2];
-                            if(new_col1 >= 0 && new_col1 < c && new_col2 >= 0 && new_col2 < c) current_cherries += dp[i+1][new_col1][new_col2];
+                            if(new_col1 >= 0 && new_col1 < c && new_col2 >= 0 && new_col2 < c) current_cherries += front[new_col1][new_col2];
                             else current_cherries += -1e9;
                             maxi = max(maxi, current_cherries);
                         }
                     }
-                    dp[i][j1][j2] = maxi;
+                    curr[j1][j2] = maxi;
                 }
             }
+
+            front = curr;
         }
         
 
-        return dp[0][0][c-1];
+        return front[0][c-1];
     }
 };
