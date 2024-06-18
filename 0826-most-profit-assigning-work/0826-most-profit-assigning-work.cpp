871b87004997811c816f36357691f6cb873b3c44
Akshit Mittal <116814372+AkshitMital@users.noreply.github.com>
2024-06-18 12:28:02 +0530

    Time: 41 ms (90.08%), Space: 40.9 MB (76.68%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.cpp
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.cpp
@@ -1,26 +1,38 @@
 class Solution {
 public:
-    int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
-        unordered_map<int,int> mpp;
-        int maxi = INT_MIN;
-        for(int i = 0; i < difficulty.size(); i++){
-            mpp[difficulty[i]] = max(mpp[difficulty[i]], profit[i]);
-            maxi = max(maxi, difficulty[i]);
-        }
-        vector<int> arr(maxi+1, 0);
-        if(mpp.find(0) != mpp.end()) arr[0] = mpp[0];
-        for(int i = 1; i <= maxi; i++){
-            arr[i] = max(arr[i - 1], mpp[i]);
-        }
-        int ans = 0;
-        for(auto it : worker){
-            if(it > maxi){
-                ans += arr[maxi];
-                continue;
-            }
-            ans += arr[it];
+    int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit,
+                            vector<int>& worker) {
+        vector<pair<int, int>> jobProfile;
+        jobProfile.push_back({0, 0});
+        for (int i = 0; i < difficulty.size(); i++) {
+            jobProfile.push_back({profit[i], difficulty[i]});
         }
 
-        return ans;
+        // Sort in decreasing order of profit.
+        sort(jobProfile.begin(), jobProfile.end());
+        reverse(jobProfile.begin(), jobProfile.end());
+        for (int i = 0; i < jobProfile.size() - 1; i++) {
+            jobProfile[i + 1].second =
+                min(jobProfile[i].second, jobProfile[i + 1].second);
+        }
+
+        int netProfit = 0;
+        for (int i = 0; i < worker.size(); i++) {
+            int ability = worker[i];
+            // Maximize profit using binary search.
+            int l = 0, r = jobProfile.size() - 1, jobProfit = 0;
+            while (l <= r) {
+                int mid = (l + r) / 2;
+                if (jobProfile[mid].second <= ability) {
+                    jobProfit = max(jobProfit, jobProfile[mid].first);
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            }
+            // Add profit of each worker to total profit.
+            netProfit += jobProfit;
+        }
+        return netProfit;
     }
 };
