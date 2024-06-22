635e46e470d26e6717df0459c4439a765ca03737
Akshit Mittal <116814372+AkshitMital@users.noreply.github.com>
2024-06-22 15:01:46 +0530

    Time: 76 ms (16.29%), Space: 15.8 MB (57.11%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -22,44 +22,13 @@
 //     }
 // };
 
-// class Solution {
-// public:
-//     bool isMatch(string s, string p) {
-//         int n = s.length(), m = p.length();
-//         vector<vector<bool>> dp(n+1, vector<bool>(m+1,false)); 
-//         dp[0][0] = true;
-//         for(int i=1; i<=n; i++) dp[i][0] = false;
-//         for(int j=1; j<=m; j++){
-//             bool flag = true;
-//             for(int k = 1; k <= j; k++){
-//                 if(p[k-1] != '*') {
-//                     flag = false;
-//                     break;
-//                 }
-//             }
-//             dp[0][j] = flag;
-//         }
-
-
-//         for(int i = 1; i <= n; i++){
-//             for(int j = 1; j <= m; j++){
-//                 if(s[i-1] == p[j-1] || p[j-1] == '?') dp[i][j] = dp[i-1][j-1];
-//                 else if(p[j-1] == '*') dp[i][j] = dp[i][j-1] | dp[i-1][j];
-//                 else dp[i][j] = false;
-//             }
-//         }
-
-
-//         return dp[n][m];
-//     }
-// };
-
 class Solution {
 public:
     bool isMatch(string s, string p) {
         int n = s.length(), m = p.length();
-        vector<bool> prev(m+1,false), curr(m+1, false); 
-        prev[0] = true;
+        vector<vector<bool>> dp(n+1, vector<bool>(m+1,false)); 
+        dp[0][0] = true;
+        for(int i=1; i<=n; i++) dp[i][0] = false;
         for(int j=1; j<=m; j++){
             bool flag = true;
             for(int k = 1; k <= j; k++){
@@ -68,20 +37,51 @@ public:
                     break;
                 }
             }
-            prev[j] = flag;
+            dp[0][j] = flag;
         }
 
 
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= m; j++){
-                if(s[i-1] == p[j-1] || p[j-1] == '?') curr[j] = prev[j-1];
-                else if(p[j-1] == '*') curr[j] = curr[j-1] | prev[j];
-                else curr[j] = false;
+                if(s[i-1] == p[j-1] || p[j-1] == '?') dp[i][j] = dp[i-1][j-1];
+                else if(p[j-1] == '*') dp[i][j] = dp[i][j-1] | dp[i-1][j];
+                else dp[i][j] = false;
             }
-            prev = curr;
         }
 
 
-        return prev[m];
+        return dp[n][m];
     }
 };
+
+// class Solution {
+// public:
+//     bool isMatch(string s, string p) {
+//         int n = s.length(), m = p.length();
+//         vector<bool> prev(m+1,false), curr(m+1, false); 
+//         prev[0] = true;
+//         for(int j=1; j<=m; j++){
+//             bool flag = true;
+//             for(int k = 1; k <= j; k++){
+//                 if(p[k-1] != '*') {
+//                     flag = false;
+//                     break;
+//                 }
+//             }
+//             prev[j] = flag;
+//         }
+
+
+//         for(int i = 1; i <= n; i++){
+//             for(int j = 1; j <= m; j++){
+//                 if(s[i-1] == p[j-1] || p[j-1] == '?') curr[j] = prev[j-1];
+//                 else if(p[j-1] == '*') curr[j] = curr[j-1] | prev[j];
+//                 else curr[j] = false;
+//             }
+//             prev = curr;
+//         }
+
+
+//         return prev[m];
+//     }
+// };
