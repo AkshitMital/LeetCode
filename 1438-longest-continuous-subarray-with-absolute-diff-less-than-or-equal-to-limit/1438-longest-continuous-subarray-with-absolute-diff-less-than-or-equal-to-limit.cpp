c16104a380506c5b3e7b851b67b807a34303fc72
Akshit Mittal <116814372+AkshitMital@users.noreply.github.com>
2024-06-23 20:07:53 +0530

    Time: 110 ms (50.8%), Space: 61.9 MB (55.61%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -3,17 +3,23 @@ public:
     int longestSubarray(vector<int>& nums, int limit) {
         int n = nums.size();
 
-        multiset<int> mst;
+        priority_queue<pair<int,int>> max_pq;
         priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> min_pq;
 
         int i = 0, j = 0, ans = 0;
 
         while(j < n){
-            mst.insert(nums[j]);
+            max_pq.push({nums[j], j});
+            min_pq.push({nums[j], j});
 
-            if(*mst.rbegin() - *mst.begin() > limit){
-                mst.erase(mst.find(nums[i]));
-                i++;
+            if(max_pq.top().first - min_pq.top().first > limit){
+                i = min(max_pq.top().second, min_pq.top().second) + 1;
+                while(max_pq.top().second < i){
+                    max_pq.pop();
+                }
+                while(min_pq.top().second < i){
+                    min_pq.pop();
+                }
             }
 
             ans = max(ans, j - i + 1);
