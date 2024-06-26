2099ab2df976eecc9057c8f2a87f8a2bbc9777cb
Akshit Mittal <116814372+AkshitMital@users.noreply.github.com>
2024-06-26 14:52:29 +0530

    Time: 85 ms (70.08%), Space: 62.7 MB (40.07%) - LeetHub

--- 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.cpp
+++ 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.cpp
@@ -11,26 +11,28 @@
  */
 class Solution {
 private:
-    void findSortedArray(TreeNode* root, vector<int>& vec){
-        if(!root) return;
-
-        findSortedArray(root->left, vec);
-        vec.push_back(root->val);
-        findSortedArray(root->right, vec);
+    void findInorder(TreeNode* root, vector<int>& nums) {
+        if (root != nullptr) {
+            findInorder(root->left, nums);
+            nums.push_back(root->val);
+            findInorder(root->right, nums);
+        }
     }
-
-    TreeNode* solve(int l, int r, vector<int> vec){
-        if(l > r) return NULL;
-        int mid = l + (r-l)/2;
-        TreeNode* root = new TreeNode(vec[mid]);
-        root->left = solve(l, mid-1, vec);
-        root->right = solve(mid+1, r, vec);
+    
+    TreeNode* makeTree(vector<int>& nums, int low, int high) {
+        if (low > high) { 
+            return nullptr;
+        }
+        int mid = low + (high - low) / 2;
+        TreeNode* root = new TreeNode(nums[mid]);
+        root->left = makeTree(nums, low, mid - 1);
+        root->right = makeTree(nums, mid + 1, high);
         return root;
     }
 public:
     TreeNode* balanceBST(TreeNode* root) {
         vector<int> inorder;
-        findSortedArray(root, inorder);
-        return solve(0, inorder.size()-1, inorder);
+        findInorder(root, inorder);
+        return makeTree(inorder, 0, inorder.size()-1);
     }
 };
