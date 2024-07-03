df444984555eec2dc70beada9040bb8e1efe658e
Akshit Mittal <116814372+AkshitMital@users.noreply.github.com>
2024-07-04 02:51:12 +0530

    Time: 3469 ms (6.88%), Space: 0B (100%) - LeetHub

--- 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,4 +1,4 @@
--- Write your PostgreSQL query statement below
+# Write your MySQL query statement below
 select v.customer_id as customer_id, count(v.visit_id) as count_no_trans 
 from visits v left join Transactions t 
 on v.visit_id = t.visit_id  
