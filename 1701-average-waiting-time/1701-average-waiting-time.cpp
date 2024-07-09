a9a3a1b2e3f48ebe3382b6b179eb1c928cb63ad5
Akshit Mittal <116814372+AkshitMital@users.noreply.github.com>
2024-07-09 12:28:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1701-average-waiting-time/1701-average-waiting-time.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    double averageWaitingTime(vector<vector<int>>& customers) {
+        double n = customers.size();
+        double consec = customers[0][0] + customers[0][1];
+        double num = customers[0][1];
+        for(int i = 1; i < n; i++){
+            if(customers[i][0] < consec) {
+                consec += customers[i][1];
+                num += (consec-customers[i][0]);
+            }
+            else {
+                consec = 0;
+                num += customers[i][1];
+            }
+        }
+        return num/n;
+    }
+};
