6d279a8a18f01f9111d561036881c87461d24949
Akshit Mittal <116814372+AkshitMital@users.noreply.github.com>
2024-07-11 16:51:37 +0530

    Time: 0 ms (100%), Space: 7.9 MB (50.36%) - LeetHub

--- 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.cpp
+++ 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -1,47 +1,33 @@
-string shit(int & i,string s,int len){
-    string b="";
-    string extra="";
-    int flag=0;
-    while(i<len){
-        if(flag==0){
-            if(s[i]=='('){
-                flag=1;
-            }
-            else if(isalpha(s[i])){
-                b+=s[i];
-            }
-        }
-        else if(flag==1){
-            if(isalpha(s[i])){
-                extra+=s[i];
-            }
-            else if(s[i]==')'){
-                reverse(extra.begin(),extra.end());
-                b+=extra;
-                //i++;
-                return b;
-            }
-            else if(s[i]=='('){
-                //i++;
-                extra+=shit(i,s,len);
-                
-            }
-        }
-        i++;
-    }
-    return b;
-}
-
 class Solution {
 public:
     string reverseParentheses(string s) {
-        string ans="";
-        int len=s.size();
-        int i=0;
-        //ans=shit(i,s,len);
-        while(i<len){
-            ans+=shit(i,s,len);
+        int n = s.length();
+        stack<int> openbracketidx;
+        vector<int> door(n);
+
+        for(int i = 0; i < n; i++){
+            if(s[i] == '('){
+                openbracketidx.push(i);
+            }
+            else if(s[i] == ')'){
+                int j = openbracketidx.top();
+                openbracketidx.pop();
+                door[i] = j;
+                door[j] = i;
+            }
         }
-        return ans;
+
+        string result;
+        int flag = 1;
+        for(int i = 0; i < n; i+=flag){
+            if(s[i] == ')' || s[i] == '('){
+                i = door[i];
+                flag = -flag;
+            }else{
+                result.push_back(s[i]);
+            }
+        }
+
+        return result;
     }
 };
