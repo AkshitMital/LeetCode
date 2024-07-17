1e5379bfed30f88c11a8f70e400a4a4d3f753313
Akshit Mittal <116814372+AkshitMital@users.noreply.github.com>
2024-07-18 02:50:24 +0530

    Time: 13 ms (43.61%), Space: 16.4 MB (50.9%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -22,51 +22,51 @@
 //     }
 // };
 
-// class Solution {
-// public:
-//     int maxProfit(int k, vector<int>& prices) {
-//         int n = prices.size();
-//         vector<vector<int>> dp(n+1, vector<int>(2*k+1, 0));
-
-//         for(int ind = n-1; ind >= 0; ind--){
-//             for(int transactions = 2*k-1; transactions >= 0; transactions--){
-//                 int price = 0;
-//                 if(transactions % 2 == 0) //buy
-//                     price = max(dp[ind+1][transactions+1] - prices[ind],
-//                                 dp[ind+1][transactions]);
-//                 else //sell
-//                     price = max(dp[ind+1][transactions+1] + prices[ind],
-//                                 dp[ind+1][transactions]);
-
-//                 dp[ind][transactions] = price;
-//             }
-//         }
-
-//         return dp[0][0];
-//     }
-// };
-
 class Solution {
 public:
     int maxProfit(int k, vector<int>& prices) {
         int n = prices.size();
-        vector<int> prev(2*k+1, 0), curr(2*k+1, 0);
+        vector<vector<int>> dp(n+1, vector<int>(2*k+1, 0));
 
         for(int ind = n-1; ind >= 0; ind--){
             for(int transactions = 2*k-1; transactions >= 0; transactions--){
                 int price = 0;
                 if(transactions % 2 == 0) //buy
-                    price = max(prev[transactions+1] - prices[ind],
-                                prev[transactions]);
+                    price = max(dp[ind+1][transactions+1] - prices[ind],
+                                dp[ind+1][transactions]);
                 else //sell
-                    price = max(prev[transactions+1] + prices[ind],
-                                prev[transactions]);
+                    price = max(dp[ind+1][transactions+1] + prices[ind],
+                                dp[ind+1][transactions]);
 
-                curr[transactions] = price;
+                dp[ind][transactions] = price;
             }
-            prev = curr;
         }
 
-        return prev[0];
+        return dp[0][0];
     }
 };
+
+// class Solution {
+// public:
+//     int maxProfit(int k, vector<int>& prices) {
+//         int n = prices.size();
+//         vector<int> prev(2*k+1, 0), curr(2*k+1, 0);
+
+//         for(int ind = n-1; ind >= 0; ind--){
+//             for(int transactions = 2*k-1; transactions >= 0; transactions--){
+//                 int price = 0;
+//                 if(transactions % 2 == 0) //buy
+//                     price = max(prev[transactions+1] - prices[ind],
+//                                 prev[transactions]);
+//                 else //sell
+//                     price = max(prev[transactions+1] + prices[ind],
+//                                 prev[transactions]);
+
+//                 curr[transactions] = price;
+//             }
+//             prev = curr;
+//         }
+
+//         return prev[0];
+//     }
+// };
