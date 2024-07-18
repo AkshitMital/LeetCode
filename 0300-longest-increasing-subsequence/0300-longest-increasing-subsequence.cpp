af62d30e9bae8134be1229a54c9dba1843de1cab
Akshit Mittal <116814372+AkshitMital@users.noreply.github.com>
2024-07-19 01:29:17 +0530

    Time: 156 ms (44.1%), Space: 13.4 MB (43.09%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,23 +1,23 @@
-class Solution {
-private:
-    int solve(int ind, int prev, vector<int>& nums, vector<vector<int>>& dp){
-        if(ind == nums.size()) return 0;
+// class Solution {
+// private:
+//     int solve(int ind, int prev, vector<int>& nums, vector<vector<int>>& dp){
+//         if(ind == nums.size()) return 0;
 
-        if(dp[ind][prev+1] != -1) return dp[ind][prev+1];
+//         if(dp[ind][prev+1] != -1) return dp[ind][prev+1];
 
-        int len = 0 + solve(ind+1, prev, nums, dp);
-        if(prev == -1 || nums[ind] > nums[prev]) 
-            len = max(len, 1 + solve(ind+1, ind, nums, dp));
+//         int len = 0 + solve(ind+1, prev, nums, dp);
+//         if(prev == -1 || nums[ind] > nums[prev]) 
+//             len = max(len, 1 + solve(ind+1, ind, nums, dp));
 
-        return dp[ind][prev+1] = len;
-    }
-public:
-    int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n+1, -1));
-        return solve(0, -1, nums, dp);
-    }
-};
+//         return dp[ind][prev+1] = len;
+//     }
+// public:
+//     int lengthOfLIS(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<vector<int>> dp(n, vector<int>(n+1, -1));
+//         return solve(0, -1, nums, dp);
+//     }
+// };
 
 // class Solution {
 // public:
@@ -39,23 +39,23 @@ public:
 //     }
 // };
 
-// class Solution {
-// public:
-//     int lengthOfLIS(vector<int>& nums) {
-//         int n = nums.size();
-//         vector<int> next(n+1, 0), curr(n+1, 0);
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> next(n+1, 0), curr(n+1, 0);
 
-//         for(int ind = n-1; ind >= 0; ind--){
-//             for(int prev = ind-1; prev >= -1; prev--){
-//                 int len = 0 + next[prev+1];
-//                 if(prev == -1 || nums[ind] > nums[prev]) 
-//                     len = max(len, 1 + next[ind+1]);
+        for(int ind = n-1; ind >= 0; ind--){
+            for(int prev = ind-1; prev >= -1; prev--){
+                int len = 0 + next[prev+1];
+                if(prev == -1 || nums[ind] > nums[prev]) 
+                    len = max(len, 1 + next[ind+1]);
 
-//                 curr[prev+1] = len;
-//             }
-//             next = curr;
-//         }
+                curr[prev+1] = len;
+            }
+            next = curr;
+        }
 
-//         return next[-1+1];
-//     }
-// };
+        return next[-1+1];
+    }
+};
