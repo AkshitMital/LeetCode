23d79b00f045ef5967f506c5722ce097459c768b
Akshit Mittal <116814372+AkshitMital@users.noreply.github.com>
2024-07-21 23:38:44 +0530

    Time: 109 ms (75.89%), Space: 12.1 MB (36.23%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -23,18 +23,6 @@
 // };
 
 class Solution {
-private:
-    int solve(int i, int j, vector<int>& nums, vector<vector<int>>& dp){
-        if(i > j) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        int maxi = INT_MIN;
-        for(int k = i; k <= j; k++){
-            int cost = nums[i-1] * nums[k] * nums[j+1] + solve(i, k-1, nums, dp) + solve(k+1, j, nums, dp);
-            maxi = max(maxi , cost);
-        }
-
-        return dp[i][j] = maxi;
-    }
 public:
     int maxCoins(vector<int>& nums) {
         int n = nums.size();
