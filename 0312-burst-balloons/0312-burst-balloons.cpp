db74aaf88a7d35c04786aecc02faaff877256b75
Akshit Mittal <116814372+AkshitMital@users.noreply.github.com>
2024-07-21 23:39:36 +0530

    Time: 110 ms (73.11%), Space: 12.2 MB (13.72%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,48 +1,48 @@
-class Solution {
-private:
-    int solve(int i, int j, vector<int>& nums, vector<vector<int>>& dp){
-        if(i > j) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        int maxi = INT_MIN;
-        for(int k = i; k <= j; k++){
-            int cost = nums[i-1] * nums[k] * nums[j+1] + solve(i, k-1, nums, dp) + solve(k+1, j, nums, dp);
-            maxi = max(maxi , cost);
-        }
-
-        return dp[i][j] = maxi;
-    }
-public:
-    int maxCoins(vector<int>& nums) {
-        int n = nums.size();
-        nums.insert(nums.begin(), 1);
-        nums.push_back(1);
-        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
-
-        return solve(1, n, nums, dp);
-    }
-};
-
 // class Solution {
+// private:
+//     int solve(int i, int j, vector<int>& nums, vector<vector<int>>& dp){
+//         if(i > j) return 0;
+//         if(dp[i][j] != -1) return dp[i][j];
+//         int maxi = INT_MIN;
+//         for(int k = i; k <= j; k++){
+//             int cost = nums[i-1] * nums[k] * nums[j+1] + solve(i, k-1, nums, dp) + solve(k+1, j, nums, dp);
+//             maxi = max(maxi , cost);
+//         }
+
+//         return dp[i][j] = maxi;
+//     }
 // public:
 //     int maxCoins(vector<int>& nums) {
 //         int n = nums.size();
 //         nums.insert(nums.begin(), 1);
 //         nums.push_back(1);
-//         vector<vector<int>> dp(n+2, vector<int>(n+2, 0));
+//         vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
 
-//         for(int i = n; i >= 1; i--){
-//             for(int j = 1; j <= n; j++){
-//                 if(i > j) continue;
-//                 int maxi = INT_MIN;
-//                 for(int k = i; k <= j; k++){
-//                     int cost = nums[i-1] * nums[k] * nums[j+1] + dp[i][k-1] + dp[k+1][j];
-//                     maxi = max(maxi , cost);
-//                 }
-
-//                 dp[i][j] = maxi;
-//             }
-//         }
-
-//         return dp[1][n];
+//         return solve(1, n, nums, dp);
 //     }
 // };
+
+class Solution {
+public:
+    int maxCoins(vector<int>& nums) {
+        int n = nums.size();
+        nums.insert(nums.begin(), 1);
+        nums.push_back(1);
+        vector<vector<int>> dp(n+2, vector<int>(n+2, 0));
+
+        for(int i = n; i >= 1; i--){
+            for(int j = 1; j <= n; j++){
+                if(i > j) continue;
+                int maxi = INT_MIN;
+                for(int k = i; k <= j; k++){
+                    int cost = nums[i-1] * nums[k] * nums[j+1] + dp[i][k-1] + dp[k+1][j];
+                    maxi = max(maxi , cost);
+                }
+
+                dp[i][j] = maxi;
+            }
+        }
+
+        return dp[1][n];
+    }
+};
