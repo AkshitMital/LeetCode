558263aa9c242ac7d9b652e6143952fd6ac06bed
Akshit Mittal <116814372+AkshitMital@users.noreply.github.com>
2024-07-26 22:19:24 +0530

    Time: 66 ms (39.5%), Space: 24.8 MB (18.82%) - LeetHub

---
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        int maxLen = 0;
+
+        for(int i = 0; i < m; i++){
+            dp[i][0] = matrix[i][0] - '0';
+            maxLen = max(maxLen, dp[i][0]);
+        }
+        for(int j = 1; j < n; j++){
+            dp[0][j] = matrix[0][j] - '0';
+            maxLen = max(maxLen, dp[0][j]);
+        }
+        
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                if(matrix[i][j] == '1'){
+                    dp[i][j] = min({dp[i-1][j], dp[i-1][j-1], dp[i][j-1]}) + 1;
+                    maxLen = max(maxLen, dp[i][j]);
+                }
+            }
+        }
+
+        return maxLen * maxLen;
+    }
+};
