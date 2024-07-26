292677d2727ad7432bfed3b68b6a1367e4464088
Akshit Mittal <116814372+AkshitMital@users.noreply.github.com>
2024-07-26 20:03:00 +0530

    Time: 196 ms (28.31%), Space: 105.8 MB (17.68%) - LeetHub

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
@@ -1,21 +1,42 @@
+// class Solution {
+// private:
+//     int solve(int ind, vector<pair<int, int>>& count, int m, int n, vector<vector<vector<int>>>& dp) {
+//         if (ind == 0) return 0;
+//         if (dp[ind][m][n] != -1) return dp[ind][m][n];
+
+//         int not_take = solve(ind - 1, count, m, n, dp);
+//         int take = INT_MIN;
+//         if (m >= count[ind - 1].first && n >= count[ind - 1].second) {
+//             take = 1 + solve(ind - 1, count, m - count[ind - 1].first, n - count[ind - 1].second, dp);
+//         }
+
+//         return dp[ind][m][n] = max(take, not_take);
+//     }
+
+// public:
+//     int findMaxForm(vector<string>& strs, int m, int n) {
+//         vector<pair<int, int>> count; // {zeroes, ones}
+//         for (int i = 0; i < strs.size(); i++) {
+//             int cnt0 = 0, cnt1 = 0;
+//             for (char ch : strs[i]) {
+//                 if (ch == '0') cnt0++;
+//                 if (ch == '1') cnt1++;
+//             }
+//             count.push_back({cnt0, cnt1});
+//         }
+
+//         vector<vector<vector<int>>> dp(strs.size() + 1, vector<vector<int>>(m + 1, vector<int>(n + 1, -1)));
+//         return solve(strs.size(), count, m, n, dp);
+//     }
+// };
+
+
 class Solution {
-private:
-    int solve(int ind, vector<pair<int, int>>& count, int m, int n, vector<vector<vector<int>>>& dp) {
-        if (ind == 0) return 0;
-        if (dp[ind][m][n] != -1) return dp[ind][m][n];
-
-        int not_take = solve(ind - 1, count, m, n, dp);
-        int take = INT_MIN;
-        if (m >= count[ind - 1].first && n >= count[ind - 1].second) {
-            take = 1 + solve(ind - 1, count, m - count[ind - 1].first, n - count[ind - 1].second, dp);
-        }
-
-        return dp[ind][m][n] = max(take, not_take);
-    }
-
 public:
     int findMaxForm(vector<string>& strs, int m, int n) {
         vector<pair<int, int>> count; // {zeroes, ones}
+        vector<vector<vector<int>>> dp(strs.size() + 1, vector<vector<int>>(m + 1, vector<int>(n + 1, 0)));
+
         for (int i = 0; i < strs.size(); i++) {
             int cnt0 = 0, cnt1 = 0;
             for (char ch : strs[i]) {
@@ -25,7 +46,20 @@ public:
             count.push_back({cnt0, cnt1});
         }
 
-        vector<vector<vector<int>>> dp(strs.size() + 1, vector<vector<int>>(m + 1, vector<int>(n + 1, -1)));
-        return solve(strs.size(), count, m, n, dp);
+        for(int ind = 1; ind <= strs.size(); ind++){
+            for(int j = 0; j <= m; j++){
+                for(int k = 0; k <= n; k++){
+                    int not_take = dp[ind - 1][j][k];
+                    int take = INT_MIN;
+                    if (j >= count[ind - 1].first && k >= count[ind - 1].second) {
+                        take = 1 + dp[ind - 1][j - count[ind - 1].first][k - count[ind - 1].second];
+                    }
+
+                    dp[ind][j][k] = max(take, not_take);
+                }
+            }
+        }
+
+        return dp[strs.size()][m][n];
     }
 };
