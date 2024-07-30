5ad0ce6c25caaa6b44ddcd2fbe0fcc75f70787a7
Akshit Mittal <116814372+AkshitMital@users.noreply.github.com>
2024-07-30 12:41:03 +0530

    Time: 84 ms (70.97%), Space: 23.7 MB (73.37%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -1,14 +1,34 @@
+// class Solution {
+// public:
+//     int minimumDeletions(string s) {
+//         stack<char> st;
+//         int ans = 0;
+//         for(int i = 0; i < s.length(); i++){
+//             if(!st.empty() && s[i] == 'a' && st.top() == 'b'){
+//                 st.pop();
+//                 ans++;
+//             }
+//             else st.push(s[i]);
+//         }
+
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     int minimumDeletions(string s) {
-        stack<char> st;
-        int ans = 0;
+        int total_a = 0;
+        for(char ch : s){
+            if(ch == 'a') total_a++;
+        }
+
+        int count_b = 0, ans = INT_MAX;
+
         for(int i = 0; i < s.length(); i++){
-            if(!st.empty() && s[i] == 'a' && st.top() == 'b'){
-                st.pop();
-                ans++;
-            }
-            else st.push(s[i]);
+            if(s[i] == 'a') total_a--;
+            ans = min(ans, total_a + count_b);
+            if(s[i] == 'b') count_b++;
         }
 
         return ans;
