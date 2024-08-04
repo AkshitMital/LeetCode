91b1b7982e01cd00ef936856ef3db353897fcb18
Akshit Mittal <116814372+AkshitMital@users.noreply.github.com>
2024-08-04 17:15:11 +0530

    Time: 93 ms (48.76%), Space: 27.4 MB (19.1%) - LeetHub

--- 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.cpp
+++ 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    int MOD = 1e9+7;
     int rangeSum(vector<int>& nums, int n, int left, int right) {
         vector<int> subset;
         for(int i = 0; i < nums.size(); i++){
@@ -14,9 +15,9 @@ public:
 
         int ans = 0;
         for(int i = left-1; i < right; i++){
-            ans += (subset[i]);
+            ans = (ans + subset[i])%MOD;
         }
 
-        return ans;
+        return ans%MOD;
     }
 };
