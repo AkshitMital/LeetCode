607411ef127f106f9c7cad850dc9407244ee1e99
Akshit Mittal <116814372+AkshitMital@users.noreply.github.com>
2024-08-06 13:35:22 +0530

    Time: 118 ms (27.64%), Space: 27.2 MB (35.85%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -4,13 +4,13 @@ public:
         unordered_map<char, int> freq;
         for(auto ch : word) freq[ch]++;
 
-        priority_queue<pair<int,char>> pq;
-        for(auto it : freq) pq.push({it.second, it.first});
+        priority_queue<int> pq;
+        for(auto it : freq) pq.push(it.second);
         int ans = 0;
         int idx = 0;
 
         while(!pq.empty()){
-            int num = pq.top().first;
+            int num = pq.top();
             pq.pop();
             ans += ((1 + idx/8) * num);
             idx++;
