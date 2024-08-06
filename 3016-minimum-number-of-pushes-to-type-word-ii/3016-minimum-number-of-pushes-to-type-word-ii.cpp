2a392acb13bd3eef08420f54b39692f6284263cd
Akshit Mittal <116814372+AkshitMital@users.noreply.github.com>
2024-08-06 13:28:46 +0530

    Time: 118 ms (27.64%), Space: 27.5 MB (25.53%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        unordered_map<char, int> freq;
+        for(auto ch : word) freq[ch]++;
+
+        priority_queue<pair<int,char>> pq;
+        for(auto it : freq) pq.push({it.second, it.first});
+        int ans = 0;
+        int mul = 1;
+        int temp = 2;
+        while(!pq.empty()){
+            char ch = pq.top().second;
+            int num = pq.top().first;
+            pq.pop();
+            ans += (num * mul);
+            temp++;
+            if(temp > 9){
+                temp = 2;
+                mul++;
+            }
+        }
+
+        return ans;
+    }
+};
