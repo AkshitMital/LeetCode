192616fc0fe2f8fd70b3a05eb167191d473fa6e4
Akshit Mittal <116814372+AkshitMital@users.noreply.github.com>
2024-08-06 13:35:15 +0530

    Time: 121 ms (25.75%), Space: 27.4 MB (25.53%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -7,19 +7,13 @@ public:
         priority_queue<pair<int,char>> pq;
         for(auto it : freq) pq.push({it.second, it.first});
         int ans = 0;
-        int mul = 1;
-        int temp = 2;
-        
+        int idx = 0;
+
         while(!pq.empty()){
-            char ch = pq.top().second;
             int num = pq.top().first;
             pq.pop();
-            ans += (num * mul);
-            temp++;
-            if(temp > 9){
-                temp = 2;
-                mul++;
-            }
+            ans += ((1 + idx/8) * num);
+            idx++;
         }
 
         return ans;
