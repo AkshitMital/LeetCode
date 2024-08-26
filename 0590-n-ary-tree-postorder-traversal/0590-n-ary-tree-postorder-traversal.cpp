f686cac4d085bb7774cb3f9d04eb972ee503752e
Akshit Mittal <116814372+AkshitMital@users.noreply.github.com>
2024-08-26 10:48:05 +0530

    Time: 17 ms (28.07%), Space: 15.1 MB (70.21%) - LeetHub

---
+++ 0590-n-ary-tree-postorder-traversal/0590-n-ary-tree-postorder-traversal.cpp
@@ -0,0 +1,37 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+private:
+    void solve(Node* root, vector<int>& ans){
+        if(!root) return;
+
+        for(Node* leaf : root->children) solve(leaf, ans);
+
+        ans.push_back(root->val);
+    }
+public:
+    vector<int> postorder(Node* root) {
+        vector<int> ans;
+        solve(root, ans);
+
+        return ans;
+    }
+};
