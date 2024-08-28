b8fd251970f4f5064cf039b6ec57d87ce0688966
Akshit Mittal <116814372+AkshitMital@users.noreply.github.com>
2024-08-29 00:49:58 +0530

    Time: 39 ms (49.91%), Space: 21.2 MB (36.78%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -1,28 +1,54 @@
+// class Solution {
+// public:
+//     int maxPoints(vector<vector<int>>& points) {
+//         int n = points.size();
+//         if (n < 2) return n;
+//         int max_points = 1;
+//         for(int i = 0; i < n-1; i++){
+//             unordered_map<double, int> slope_freq;
+//             int verticals = 0;
+//             int duplicates = 0;
+//             for(int j = i + 1; j < n; j++){
+//                 auto p1 = points[i];
+//                 auto p2 = points[j];
+
+//                 if (p1 == p2) duplicates++;
+//                 else if(p1[0] == p2[0]) verticals++;
+//                 else{
+//                     double slope = double(p2[1] - p1[1])/double(p2[0] - p1[0]);
+//                     slope_freq[slope]++;
+//                 }
+
+//             }
+//             int temp = verticals;
+//             for(auto [slope, freq] : slope_freq) temp = max(temp, freq);
+//             max_points = max(max_points, temp + duplicates + 1);
+//         }
+
+//         return max_points;
+//     }
+// };
+
 class Solution {
 public:
     int maxPoints(vector<vector<int>>& points) {
+        unordered_map<double, int> slope_freq;
         int n = points.size();
-        if (n < 2) return n;
         int max_points = 1;
         for(int i = 0; i < n-1; i++){
-            unordered_map<double, int> slope_freq;
-            int verticals = 0;
-            int duplicates = 0;
             for(int j = i + 1; j < n; j++){
                 auto p1 = points[i];
                 auto p2 = points[j];
+                double dx = double(p2[0] - p1[0]), dy = double(p2[1] - p1[1]), slope;
+                
+                if(dx == 0) slope = INT_MAX;
+                else slope = dy/dx;
 
-                if (p1 == p2) duplicates++;
-                else if(p1[0] == p2[0]) verticals++;
-                else{
-                    double slope = double(p2[1] - p1[1])/double(p2[0] - p1[0]);
-                    slope_freq[slope]++;
-                }
+                slope_freq[slope]++;
+                max_points = max(max_points, slope_freq[slope] + 1);
 
             }
-            int temp = verticals;
-            for(auto [slope, freq] : slope_freq) temp = max(temp, freq);
-            max_points = max(max_points, temp + duplicates + 1);
+            slope_freq.clear();
         }
 
         return max_points;
