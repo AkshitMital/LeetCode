ff134ebd12c77618a3c9994334266a25ddc95668
Akshit Mittal <116814372+AkshitMital@users.noreply.github.com>
2024-08-29 00:49:51 +0530

    Time: 43 ms (42.94%), Space: 23 MB (27.91%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -2,22 +2,27 @@ class Solution {
 public:
     int maxPoints(vector<vector<int>>& points) {
         int n = points.size();
-        int max_points = 0;
+        if (n < 2) return n;
+        int max_points = 1;
         for(int i = 0; i < n-1; i++){
-            map<double, int> slope_freq;
+            unordered_map<double, int> slope_freq;
+            int verticals = 0;
+            int duplicates = 0;
             for(int j = i + 1; j < n; j++){
                 auto p1 = points[i];
                 auto p2 = points[j];
 
-                double slope = 0.0;
-                if(p1[0] != p2[0]){
-                    slope = double(abs(p2[1] - p1[1]))/double(abs(p2[0] - p1[0]));
+                if (p1 == p2) duplicates++;
+                else if(p1[0] == p2[0]) verticals++;
+                else{
+                    double slope = double(p2[1] - p1[1])/double(p2[0] - p1[0]);
                     slope_freq[slope]++;
                 }
+
             }
-            int temp = 0;
-            for(auto [slope, freq] : slope_freq) temp = max(temp, freq+1);
-            max_points = max(temp, max_points);
+            int temp = verticals;
+            for(auto [slope, freq] : slope_freq) temp = max(temp, freq);
+            max_points = max(max_points, temp + duplicates + 1);
         }
 
         return max_points;
