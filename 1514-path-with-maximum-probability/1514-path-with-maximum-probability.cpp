f96168c97de8a12de223a2a74380050330ff6019
Akshit Mittal <116814372+AkshitMital@users.noreply.github.com>
2024-08-31 13:22:16 +0530

    Time: 124 ms (53.58%), Space: 70.2 MB (48.2%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
     double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start_node, int end_node) {
-        vector<vector<pair<int,double>>> adj(n);
+        vector<vector<pair<int, double>>> adj(n);
         for(int i = 0; i < edges.size(); i++){
             int u = edges[i][0];
             int v = edges[i][1];
             double wt = succProb[i];
-            adj[u].push_back({v, wt});
-            adj[v].push_back({u, wt});
+            adj[u].push_back({v,wt});
+            adj[v].push_back({u,wt});
         }
 
-        vector<double> max_prob(n, 0.0);
         priority_queue<pair<double,int>> pq;
+        vector<double> max_prob(n, 0.0);
         pq.push({1.0, start_node});
         max_prob[start_node] = 1.0;
 
         while(!pq.empty()){
-            double prob = pq.top().first;
-            int node = pq.top().second;
+            double currProb = pq.top().first;
+            int currNode = pq.top().second;
             pq.pop();
 
-            for(auto& it : adj[node]){
-                double temp_prob = it.second;
-                int adjNode = it.first;
-
-                if(prob*temp_prob > max_prob[adjNode]){
-                    max_prob[adjNode] = prob*temp_prob;
-                    pq.push({max_prob[adjNode], adjNode});
+            for(auto& [node, prob] : adj[currNode]){
+                if(prob*currProb > max_prob[node]){
+                    max_prob[node] = prob*currProb;
+                    pq.push({max_prob[node], node});
                 }
             }
         }
