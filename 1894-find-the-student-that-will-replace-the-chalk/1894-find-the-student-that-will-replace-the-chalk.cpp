bb23ef7577cb663119d5191ccdf29bd059384e49
Akshit Mittal <116814372+AkshitMital@users.noreply.github.com>
2024-09-02 14:45:14 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.cpp
+++ 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.cpp
@@ -4,19 +4,12 @@ public:
         int n = chalk.size(), i;
         long long sum = 0;
         for(auto it : chalk) sum += it;
-        if(k < sum){
-            for(i = 0; i <= n; i++){
-                if(k < chalk[i]) break;
-                k -= chalk[i];
-            }
-        }
-        else if(k == sum) return 0;
-        else{
-            k %= sum;
-            for(i = 0; i <= n; i++){
-                if(k < chalk[i]) break;
-                k -= chalk[i];
-            }
+
+        if(sum < k) k %= sum;
+
+        for(i = 0; i < n; i++){
+            if(k < chalk[i]) break;
+            k -= chalk[i];
         }
 
         return i;
