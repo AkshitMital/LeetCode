565c520a662e7a296ee70a036eccb4854be39e6a
Akshit Mittal <116814372+AkshitMital@users.noreply.github.com>
2024-09-23 18:26:33 +0530

    Time: 162 ms (39.02%), Space: 86.8 MB (52.7%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
@@ -1,3 +1,31 @@
+// class Solution {
+// private:
+//     int solve(int index, string& s, unordered_set<string>& st, vector<int>& dp, int& n){
+//         if(index >= n) return 0;
+
+//         if(dp[index] != -1) return dp[index];
+
+//         int result = 1 + solve(index + 1, s, st, dp, n);
+
+//         for(int j = index; j < n; j++){
+//             string curr = s.substr(index, j-index+1);
+//             if(st.find(curr) != st.end()){
+//                 result = min(result, solve(j+1, s, st, dp, n));
+//             }
+//         }
+
+//         return dp[index] = result;
+//     }
+// public:
+//     int minExtraChar(string s, vector<string>& dictionary) {
+//         int n = s.length();
+//         vector<int> dp(n, -1);
+//         unordered_set<string> st(begin(dictionary), end(dictionary));
+
+//         return solve(0, s, st, dp, n);
+//     }
+// };
+
 class Solution {
 private:
     int solve(int index, string& s, unordered_set<string>& st, vector<int>& dp, int& n){
@@ -19,9 +47,22 @@ private:
 public:
     int minExtraChar(string s, vector<string>& dictionary) {
         int n = s.length();
-        vector<int> dp(n, -1);
+        vector<int> dp(n+1, 0);
         unordered_set<string> st(begin(dictionary), end(dictionary));
 
-        return solve(0, s, st, dp, n);
+        for(int index = n-1; index >= 0; index--){
+            int result = 1 + dp[index+1];
+
+            for(int j = index; j < n; j++){
+                string curr = s.substr(index, j-index+1);
+                if(st.find(curr) != st.end()){
+                    result = min(result, dp[j+1]);
+                }
+            }
+
+            dp[index] = result;
+        }
+
+        return dp[0];
     }
 };
