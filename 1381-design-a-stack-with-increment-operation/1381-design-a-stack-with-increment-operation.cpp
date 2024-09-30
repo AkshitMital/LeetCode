793a696e72b0437382c2b3c34bcaf885884c1731
Akshit Mittal <116814372+AkshitMital@users.noreply.github.com>
2024-09-30 16:39:15 +0530

    Time: 26 ms (69.74%), Space: 26 MB (29.82%) - LeetHub

--- 1381-design-a-stack-with-increment-operation/1381-design-a-stack-with-increment-operation.cpp
+++ 1381-design-a-stack-with-increment-operation/1381-design-a-stack-with-increment-operation.cpp
@@ -1,31 +1,64 @@
+// class CustomStack {
+// private:
+//     vector<int> arr;
+//     int top;
+// public:
+//     CustomStack(int maxSize) {
+//         arr.resize(maxSize);
+//         top = -1;
+//     }
+    
+//     void push(int x) {
+//         if(top == arr.size()-1) return;
+//         top += 1;
+//         arr[top] = x;
+//     }
+    
+//     int pop() {
+//         if(top == -1) return -1;
+//         int ans = arr[top];
+//         top--;
+//         return ans;
+//     }
+    
+//     void increment(int k, int val) {
+//         for(int i = 0; i < min(k,top+1); i++){
+//             arr[i] += val;
+//         }
+//     }
+// };
+
 class CustomStack {
 private:
     vector<int> arr;
-    int top;
+    vector<int> increments;
+    int top, n;
 public:
     CustomStack(int maxSize) {
+        n = maxSize;
         arr.resize(maxSize);
+        increments.resize(maxSize);
         top = -1;
     }
     
     void push(int x) {
-        if(top == arr.size()-1) return;
+        if(top == n-1) return;
         top += 1;
         arr[top] = x;
     }
     
     int pop() {
         if(top == -1) return -1;
-        int ans = arr[top];
+        int ans = arr[top] + increments[top];
+        if(top-1 >= 0) increments[top-1] += increments[top];
+        increments[top] = 0;
         top--;
         return ans;
     }
     
     void increment(int k, int val) {
-        for(int i = 0; i < min(k,top+1); i++){
-            // if(i > top) break;
-            arr[i] += val;
-        }
+        int idx = min(k-1, top);
+        if(idx >= 0) increments[idx] += val;
     }
 };
 
