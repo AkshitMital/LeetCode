82fe709b673c2017149c2b40cda1933a16352b43
Akshit Mittal <116814372+AkshitMital@users.noreply.github.com>
2024-10-01 22:12:31 +0530

    Time: 2 ms (90.38%), Space: 13.8 MB (45.31%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -1,12 +1,12 @@
 class Solution {
 private:
-    void solve(int currNum, int n, vector<int>& res){
+    void solve(int currNum, int& n, vector<int>& res){
         if(currNum > n) return;
 
         res.push_back(currNum);
 
-        for(int append = 0; append <= 9; append++){
-            int newNum = currNum*10 + append;
+        for(int add = 0; add <= 9; add++){
+            int newNum = currNum * 10 + add;
 
             if(newNum > n) return;
 
