eae0a2d5f39b7a323f2871d79fb8c74efa2a5964
Akshit Mittal <116814372+AkshitMital@users.noreply.github.com>
2024-10-05 13:25:21 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0641-design-circular-deque/0641-design-circular-deque.cpp
+++ 0641-design-circular-deque/0641-design-circular-deque.cpp
@@ -1,57 +1,182 @@
+// class MyCircularDeque {
+// private:
+//     vector<int> queue;
+//     int front;
+//     int rear;
+//     int size;
+//     int cap;
+// public:
+//     MyCircularDeque(int k) {
+//         queue.resize(k);
+//         cap = k;
+//         front = 0;
+//         rear = k-1;
+//         size = 0;
+//     }
+    
+//     bool insertFront(int value) {
+//         if(isFull()) return false;
+//         front = (front - 1 + cap) % cap;
+//         queue[front] = value;
+//         size++;
+//         return true;
+//     }
+    
+//     bool insertLast(int value) {
+//         if(isFull()) return false;
+//         rear = (rear + 1) % cap;
+//         queue[rear] = value;
+//         size++;
+//         return true;
+//     }
+    
+//     bool deleteFront() {
+//         if(isEmpty()) return false;
+//         front = (front + 1) % cap;
+//         size--;
+//         return true;
+//     }
+    
+//     bool deleteLast() {
+//         if(isEmpty()) return false;
+//         rear = (rear - 1 + cap) % cap;
+//         size--;
+//         return true;
+//     }
+    
+//     int getFront() {
+//         if(isEmpty()) return -1;
+//         return queue[front];
+//     }
+    
+//     int getRear() {
+//         if(isEmpty()) return -1;
+//         return queue[rear];
+//     }
+    
+//     bool isEmpty() {
+//         return (size == 0);
+//     }
+    
+//     bool isFull() {
+//         return (size == cap);
+//     }
+// };
+
+class Node{
+private:
+    int val;
+    Node *next, *prev;
+    Node(int val){
+        this->val = val;
+        next = prev = NULL;
+    }
+    friend class MyCircularDeque;
+};
+
 class MyCircularDeque {
 private:
-    vector<int> queue;
-    int front;
-    int rear;
-    int size;
-    int cap;
+    Node *head, *tail;
+    int size, cap;
 public:
     MyCircularDeque(int k) {
-        queue.resize(k);
-        cap = k;
-        front = 0;
-        rear = k-1;
         size = 0;
+        cap = k;
+        head = tail = NULL;
     }
     
     bool insertFront(int value) {
         if(isFull()) return false;
-        front = (front - 1 + cap) % cap;
-        queue[front] = value;
+
+        Node* newNode = new Node(value);
+
+        if(head){
+            newNode->next = head;
+            head->prev = newNode;
+            head = head->prev;
+            head->prev = tail;
+            tail->next = head;
+        }else{
+            head = tail = newNode;
+            head->prev = tail;
+            tail->next = head;
+        }
+
         size++;
+
         return true;
     }
     
     bool insertLast(int value) {
         if(isFull()) return false;
-        rear = (rear + 1) % cap;
-        queue[rear] = value;
+
+        Node* newNode = new Node(value);
+        
+        if(tail){
+            newNode->prev = tail;
+            tail->next = newNode;
+            tail = tail->next;
+            head->prev = tail;
+            tail->next = head;
+        }else{
+            head = tail = newNode;
+            head->prev = tail;
+            tail->next = head;
+        }
+
         size++;
+
         return true;
     }
     
     bool deleteFront() {
         if(isEmpty()) return false;
-        front = (front + 1) % cap;
+
+        Node* temp = head;
+
+        if(head->next){
+            head = head->next;
+            head->prev = tail;
+            tail->next = head;
+        }else{
+            head = tail = NULL;
+        }
+
+        delete temp;
+
         size--;
+
         return true;
     }
     
     bool deleteLast() {
         if(isEmpty()) return false;
-        rear = (rear - 1 + cap) % cap;
+
+        Node* temp = tail;
+
+        if(tail->prev){
+            tail = tail->prev;
+            head->prev = tail;
+            tail->next = head;
+        }else{
+            head = tail = NULL;
+        }
+
+        delete temp;
+
         size--;
+
         return true;
     }
     
     int getFront() {
         if(isEmpty()) return -1;
-        return queue[front];
+        return head->val;
     }
     
     int getRear() {
         if(isEmpty()) return -1;
-        return queue[rear];
+        return tail->val;
     }
     
     bool isEmpty() {
