9e9af0e20a7b20570114ed30c896534d6e52a720
Akshit Mittal <116814372+AkshitMital@users.noreply.github.com>
2024-10-06 17:28:41 +0530

    Time: 4 ms (40.09%), Space: 8.8 MB (16.13%) - LeetHub

--- 1813-sentence-similarity-iii/1813-sentence-similarity-iii.cpp
+++ 1813-sentence-similarity-iii/1813-sentence-similarity-iii.cpp
@@ -1,31 +1,67 @@
+// class Solution {
+// private:
+//     bool solve(string s1, string s2){
+//         vector<string> vec1, vec2;
+
+//         stringstream ss1(s1);
+//         string token;
+//         while(ss1 >> token) vec1.push_back(token);
+
+//         stringstream ss2(s2);
+//         while(ss2 >> token) vec2.push_back(token);
+
+//         int i = 0, j = vec1.size()-1, k = 0, l = vec2.size()-1;
+
+//         while(i < vec1.size() && k < vec2.size() && vec1[i] == vec2[k]){
+//             i++;
+//             k++;
+//         }
+
+//         while(j >= 0 && l >= 0 && vec1[j] == vec2[l]){
+//             j--;
+//             l--;
+//         }
+
+//         if(l < k) return true;
+
+//         return false;
+//     }
+// public:
+//     bool areSentencesSimilar(string s1, string s2) {
+//         if(s1.length() > s2.length()) 
+//             return solve(s1, s2);
+        
+//         return solve(s2, s1);
+//     }
+// };
+
 class Solution {
 private:
     bool solve(string s1, string s2){
-        vector<string> vec1, vec2;
+        deque<string> deq1, deq2;
 
         stringstream ss1(s1);
         string token;
-        while(ss1 >> token) vec1.push_back(token);
+        while(ss1 >> token) deq1.push_back(token);
 
         stringstream ss2(s2);
-        while(ss2 >> token) vec2.push_back(token);
+        while(ss2 >> token) deq2.push_back(token);
 
-        int i = 0, j = vec1.size()-1, k = 0, l = vec2.size()-1;
-
-        while(i < vec1.size() && k < vec2.size() && vec1[i] == vec2[k]){
-            i++;
-            k++;
+        while(!deq2.empty() && deq1.front() == deq2.front()){
+            deq1.pop_front();
+            deq2.pop_front();
         }
 
-        while(j >= 0 && l >= 0 && vec1[j] == vec2[l]){
-            j--;
-            l--;
+        while(!deq2.empty() && deq1.back() == deq2.back()){
+            deq1.pop_back();
+            deq2.pop_back();
         }
 
-        if(l < k) return true;
+        if(deq2.empty()) return true;
 
         return false;
     }
+    
 public:
     bool areSentencesSimilar(string s1, string s2) {
         if(s1.length() > s2.length()) 
