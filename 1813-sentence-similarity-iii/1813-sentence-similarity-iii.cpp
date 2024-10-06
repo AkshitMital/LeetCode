b873c5f3b20a3a6742451cc1da3c102b29e112a2
Akshit Mittal <116814372+AkshitMital@users.noreply.github.com>
2024-10-06 17:23:04 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1813-sentence-similarity-iii/1813-sentence-similarity-iii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+private:
+    bool solve(string s1, string s2){
+        vector<string> vec1, vec2;
+
+        stringstream ss1(s1);
+        string token;
+        while(ss1 >> token) vec1.push_back(token);
+
+        stringstream ss2(s2);
+        while(ss2 >> token) vec2.push_back(token);
+
+        int i = 0, j = s1.length()-1, k = 0, l = s2.length()-1;
+
+        while(s1[i] == s2[k]){
+            i++;
+            k++;
+        }
+
+        while(s1[j] == s2[l]){
+            j--;
+            l--;
+        }
+
+        if(l < k) return true;
+
+        return false;
+    }
+public:
+    bool areSentencesSimilar(string s1, string s2) {
+        if(s1.length() > s2.length()) 
+            return solve(s1, s2);
+        
+        return solve(s2, s1);
+    }
+};
