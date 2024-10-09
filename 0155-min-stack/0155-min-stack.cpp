f5ccbe920fb361ff0ce04db7aa7fc9b619036cc4
Akshit Mittal <116814372+AkshitMital@users.noreply.github.com>
2024-10-09 16:58:42 +0530

    Time: 16 ms (73.03%), Space: 21.4 MB (82.51%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,52 +1,99 @@
-class Node{
-private:
-    pair<int,int> val;
-    Node* next;
-    friend class MinStack;
-public:
-    Node(pair<int,int> val){
-        this->val = val;
-        next = NULL;
-    }
-};
+// class Node{
+// private:
+//     pair<int,int> val;
+//     Node* next;
+//     friend class MinStack;
+// public:
+//     Node(pair<int,int> val){
+//         this->val = val;
+//         next = NULL;
+//     }
+// };
+
+// class MinStack {
+// Node* front;
+// int mini;
+// public:
+//     MinStack() {
+//         front = NULL;
+//         mini = INT_MAX;
+//     }
+    
+//     void push(int val) {
+//         mini = min(val, mini);
+//         Node* newNode = new Node({val, mini});
+//         newNode->next = front;
+//         front = newNode;
+//         return;
+//     }
+    
+//     void pop() {
+//         if (front == NULL) return;
+
+//         Node* temp = front;
+//         front = front->next;
+//         delete temp;
+
+//         if (front == NULL) {
+//             mini = INT_MAX;
+//         } else {
+//             mini = front->val.second;
+//         }
+//     }
+    
+//     int top() {
+//         return front->val.first;
+//     }
+    
+//     int getMin() {
+//         return front->val.second;
+//     }
+// };
 
 class MinStack {
-Node* front;
-int mini;
+private:
+    stack<long long> st;
+    long long mini;
 public:
     MinStack() {
-        front = NULL;
         mini = INT_MAX;
     }
     
     void push(int val) {
-        mini = min(val, mini);
-        Node* newNode = new Node({val, mini});
-        newNode->next = front;
-        front = newNode;
-        return;
+        if(st.empty()){
+            mini = val;
+            st.push(val);
+        }else{
+            if(mini < val) st.push(val);
+            else{
+                st.push(val - mini + val);
+                mini = val;
+            }
+        }
     }
     
     void pop() {
-        if (front == NULL) return;
+        if (st.empty()) return;
 
-        Node* temp = front;
-        front = front->next;
-        delete temp;
+        long long topVal = st.top();
+        st.pop();
 
-        if (front == NULL) {
-            mini = INT_MAX;
-        } else {
-            mini = front->val.second;
+        if (topVal < mini) {
+            mini = (2 * mini) - topVal;
         }
     }
     
     int top() {
-        return front->val.first;
+        if(st.empty()) return INT_MAX;
+        long long topVal = st.top();
+        if(topVal > mini){
+            return topVal;
+        }
+        return mini;
     }
     
     int getMin() {
-        return front->val.second;
+        return mini;
     }
 };
 
