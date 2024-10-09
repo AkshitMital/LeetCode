2f44b52ce98d52946a58b9d51a624f152bf221ca
Akshit Mittal <116814372+AkshitMital@users.noreply.github.com>
2024-10-09 17:42:32 +0530

    Time: 8 ms (32.61%), Space: 12.5 MB (7.16%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -3,11 +3,11 @@ private:
     vector<int> solve(vector<int>& nums){
         stack<int> st;
         vector<int> nge(nums.size(), -1);
-        int i = nums.size() - 1;
         for(int i = nums.size()-1; i >= 0; i--){
-            while(!st.empty() && st.top() < nums[i]) st.pop();
+            while(!st.empty() && st.top() <= nums[i]) st.pop();
             if(st.empty()) nge[i] = -1;
             else nge[i] = st.top();
+            
             st.push(nums[i]);
         }
 
