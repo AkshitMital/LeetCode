6db3cce5efbecf35f1d72fdc5979bce46584cae1
Akshit Mittal <116814372+AkshitMital@users.noreply.github.com>
2024-10-13 00:27:03 +0530

    Time: 95 ms (90.74%), Space: 77.9 MB (97.54%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,42 +1,71 @@
 class Solution {
-private:
-    vector<int> computeNSE(vector<int>& heights, int n){
-        vector<int> nse(n, n);
-        stack<int> st;
-        for(int i = n-1; i >= 0; i--){
-            while(!st.empty() && heights[i] <= heights[st.top()]) st.pop();
-            if(st.empty()) nse[i] = n;
-            else nse[i] = st.top();
-            st.push(i);
-        }
-        return nse;
-    }
+// private:
+//     vector<int> computeNSE(vector<int>& heights, int n){
+//         vector<int> nse(n, n);
+//         stack<int> st;
+//         for(int i = n-1; i >= 0; i--){
+//             while(!st.empty() && heights[i] <= heights[st.top()]) st.pop();
+//             if(st.empty()) nse[i] = n;
+//             else nse[i] = st.top();
+//             st.push(i);
+//         }
+//         return nse;
+//     }
 
-    vector<int> computePSE(vector<int>& heights, int n){
-        vector<int> pse(n, 0);
-        stack<int> st;
-        for(int i = 0; i < n; i++){
-            while(!st.empty() && heights[i] <= heights[st.top()]) st.pop();
-            if(st.empty()) pse[i] = -1;
-            else pse[i] = st.top();
-            st.push(i);
-        }
-        return pse;
-    }
+//     vector<int> computePSE(vector<int>& heights, int n){
+//         vector<int> pse(n, 0);
+//         stack<int> st;
+//         for(int i = 0; i < n; i++){
+//             while(!st.empty() && heights[i] <= heights[st.top()]) st.pop();
+//             if(st.empty()) pse[i] = -1;
+//             else pse[i] = st.top();
+//             st.push(i);
+//         }
+//         return pse;
+//     }
 public:
+//     int largestRectangleArea(vector<int>& heights) {
+//         int n = heights.size();
+//         vector<int> nse = computeNSE(heights, n);
+//         vector<int> pse = computePSE(heights, n);
+
+//         for(int i = 0; i < n; i++){
+//             cout << nse[i] << " " << pse[i] << endl;
+//         }
+
+//         int maxArea = 0;
+//         for(int i = 0; i < n; i++){
+//             int area = (nse[i] - pse[i] - 1) * heights[i];
+//             maxArea = max(maxArea, area);
+//         }
+
+//         return maxArea;
+//     }
+
     int largestRectangleArea(vector<int>& heights) {
         int n = heights.size();
-        vector<int> nse = computeNSE(heights, n);
-        vector<int> pse = computePSE(heights, n);
-
-        for(int i = 0; i < n; i++){
-            cout << nse[i] << " " << pse[i] << endl;
-        }
-
+        stack<int> st;
         int maxArea = 0;
         for(int i = 0; i < n; i++){
-            int area = (nse[i] - pse[i] - 1) * heights[i];
-            maxArea = max(maxArea, area);
+            while(!st.empty() && heights[st.top()] >= heights[i]){
+                int top = st.top();
+                st.pop();
+                int nse = i;
+                int pse = (st.empty()) ? -1 : st.top();
+                int tempArea = heights[top] * (nse - pse - 1);
+                maxArea = max(tempArea, maxArea);
+            }
+
+            st.push(i);
+        }
+
+        while(!st.empty()){
+            int top = st.top();
+            st.pop();
+            int nse = n;
+            int pse = (st.empty()) ? -1 : st.top();
+            int tempArea = heights[top] * (nse - pse - 1);
+            maxArea = max(tempArea, maxArea);
         }
 
         return maxArea;
