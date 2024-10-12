21ea5fc53a7403db0301eeed0084ca00b990323a
Akshit Mittal <116814372+AkshitMital@users.noreply.github.com>
2024-10-13 00:26:46 +0530

    Time: 499 ms (5.01%), Space: 84.4 MB (33.09%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,25 +1,42 @@
 class Solution {
-public:
-    int largestRectangleArea(vector<int>& heights) {
+private:
+    vector<int> computeNSE(vector<int>& heights, int n){
+        vector<int> nse(n, n);
         stack<int> st;
-        int n = heights.size(), maxArea = INT_MIN;
-        for(int i = 0; i < n; i++){
-            while(!st.empty() && heights[st.top()] > heights[i]){
-                int element = st.top();
-                st.pop();
-                int nse = i;
-                int pse = (!st.empty()) ? st.top() : -1;
-                maxArea = max(maxArea, heights[element] * (nse-pse-1));
-            }
+        for(int i = n-1; i >= 0; i--){
+            while(!st.empty() && heights[i] <= heights[st.top()]) st.pop();
+            if(st.empty()) nse[i] = n;
+            else nse[i] = st.top();
             st.push(i);
         }
+        return nse;
+    }
 
-        while(!st.empty()){
-            int nse = n;
-            int element = st.top();
-            st.pop();
-            int pse = (!st.empty()) ? st.top() : -1;
-            maxArea = max(maxArea, heights[element] * (nse-pse-1));
+    vector<int> computePSE(vector<int>& heights, int n){
+        vector<int> pse(n, 0);
+        stack<int> st;
+        for(int i = 0; i < n; i++){
+            while(!st.empty() && heights[i] <= heights[st.top()]) st.pop();
+            if(st.empty()) pse[i] = -1;
+            else pse[i] = st.top();
+            st.push(i);
+        }
+        return pse;
+    }
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        vector<int> nse = computeNSE(heights, n);
+        vector<int> pse = computePSE(heights, n);
+
+        for(int i = 0; i < n; i++){
+            cout << nse[i] << " " << pse[i] << endl;
+        }
+
+        int maxArea = 0;
+        for(int i = 0; i < n; i++){
+            int area = (nse[i] - pse[i] - 1) * heights[i];
+            maxArea = max(maxArea, area);
         }
 
         return maxArea;
