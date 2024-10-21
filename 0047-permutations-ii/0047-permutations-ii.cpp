d389d3ebcb576f6938f61b668fa46978b7eb52e6
Akshit Mittal <116814372+AkshitMital@users.noreply.github.com>
2024-10-22 00:24:53 +0530

    Time: 6 ms (59.31%), Space: 11.8 MB (47%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,31 +1,21 @@
 class Solution {
 private:
-    void solve(vector<int>& temp, unordered_set<int>& st, set<vector<int>>& Ansst, vector<int>& nums){
-        if(temp.size() == nums.size()){
-            Ansst.insert(temp);
+    void solve(int ind, vector<int> nums, vector<vector<int>>& ans){
+        if(ind == nums.size()){
+            ans.push_back(nums);
             return;
         }
-
-        for(int i = 0; i < nums.size(); i++){
-            if(st.find(i) == st.end()){
-                st.insert(i);
-                temp.push_back(nums[i]);
-                solve(temp, st, Ansst, nums);
-                temp.pop_back();
-                st.erase(i);
-            }
+        for(int i = ind; i < nums.size(); i++){
+            if(i!=ind && nums[i] == nums[ind]) continue;
+            swap(nums[ind], nums[i]);
+            solve(ind+1, nums, ans);
         }
     }
 public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
         vector<vector<int>> ans;
-        unordered_set<int> st;
-        set<vector<int>> Ansst;
-        vector<int> temp;
-        solve(temp, st, Ansst, nums);
-
-        for(auto num : Ansst) ans.push_back(num);
-
+        solve(0, nums, ans);
         return ans;
     }
 };
