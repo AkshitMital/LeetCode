cdcc89c551c4bb9336014f26c95ebf4b9145d8ae
Akshit Mittal <116814372+AkshitMital@users.noreply.github.com>
2024-10-30 22:22:48 +0530

    Time: 8 ms (5.35%), Space: 11.1 MB (96.46%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -20,23 +20,42 @@
 //     }
 // };
 
+// class Solution {
+// public:
+//     int minimumTotal(vector<vector<int>>& triangle) {
+//         int n = triangle.size();
+//         vector<vector<int>> dp(n, vector<int>(n,0));
+
+//         for(int i = n-1; i >= 0; i--){
+//             for(int j = i; j >= 0; j--){
+//                 if(i == n-1) dp[i][j] = triangle[i][j];
+//                 else{
+//                     int down = triangle[i][j] + dp[i+1][j];
+//                     int diagnol = triangle[i][j] + dp[i+1][j+1];
+//                     dp[i][j] = min(diagnol, down);
+//                 }
+//             }
+//         }
+
+//         return dp[0][0];
+//     }
+// };
+
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<vector<int>> dp(n, vector<int>(n,0));
-
-        for(int i = n-1; i >= 0; i--){
+        vector<int> front(n), curr(n);
+        for(int j = 0; j < n; j++) front[j] = triangle[n-1][j];
+        for(int i = n-2; i >= 0; i--){
             for(int j = i; j >= 0; j--){
-                if(i == n-1) dp[i][j] = triangle[i][j];
-                else{
-                    int down = triangle[i][j] + dp[i+1][j];
-                    int diagnol = triangle[i][j] + dp[i+1][j+1];
-                    dp[i][j] = min(diagnol, down);
-                }
+                int down = triangle[i][j] + front[j];
+                int diagnol = triangle[i][j] + front[j+1];
+                curr[j] = min(diagnol, down);
             }
+            front = curr;
         }
 
-        return dp[0][0];
+        return front[0];
     }
 };
