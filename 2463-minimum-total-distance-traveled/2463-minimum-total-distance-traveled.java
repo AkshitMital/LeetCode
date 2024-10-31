d8619cb8fc036834ef6da3a2c00b20907e2dfa40
Akshit Mittal <116814372+AkshitMital@users.noreply.github.com>
2024-10-31 14:20:39 +0530

    Time: 52 ms (60%), Space: 55.2 MB (33.85%) - LeetHub

--- 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.java
+++ 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.java
@@ -1,15 +1,4 @@
 class Solution {
-    private long solve(int r, int f, List<Integer> robot, List<Integer> factories, long[][] dp){
-        if(r > 0 && f == 0) return (long)1e12;
-        if(r == 0 || f == 0) return 0;
-
-        if(dp[r][f] != -1) return dp[r][f];
-
-        long pick = Math.abs(factories.get(f-1) - robot.get(r-1)) + solve(r - 1, f - 1, robot, factories, dp);
-        long not_pick = solve(r, f - 1, robot, factories, dp);
-
-        return dp[r][f] = Math.min(pick, not_pick);
-    }
     public long minimumTotalDistance(List<Integer> robot, int[][] factory) {
         List<Integer> factories = new ArrayList<>();
         for(int[] row : factory){
@@ -30,8 +19,7 @@ class Solution {
         }
 
         long[][] dp = new long[rSize + 1][fSize + 1];
-        for(int r = 0; r <= rSize; r++) Arrays.fill(dp[r], (long) 1e12);
-        for(int f = 0; f <= fSize; f++) dp[0][f] = 0;
+        for(int r = 1; r <= rSize; r++) dp[r][0] = (long) 1e12;
 
         for(int r = 1; r <= rSize; r++){
             for(int f = 1; f <= fSize; f++){
