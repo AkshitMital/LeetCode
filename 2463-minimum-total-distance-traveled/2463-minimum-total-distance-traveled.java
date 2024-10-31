48f9f4eea4c2ee526cf2e1ed6d54500482e1d11d
Akshit Mittal <116814372+AkshitMital@users.noreply.github.com>
2024-10-31 14:11:13 +0530

    Time: 123 ms (20%), Space: 56.5 MB (30.77%) - LeetHub

--- 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.java
+++ 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.java
@@ -1,11 +1,11 @@
 class Solution {
     private long solve(int r, int f, List<Integer> robot, List<Integer> factories, long[][] dp){
-        if(r >= 0 && f == -1) return (long)1e12;
-        if(r == -1 || f == -1) return 0;
+        if(r > 0 && f == 0) return (long)1e12;
+        if(r == 0 || f == 0) return 0;
 
         if(dp[r][f] != -1) return dp[r][f];
 
-        long pick = Math.abs(factories.get(f) - robot.get(r)) + solve(r - 1, f - 1, robot, factories, dp);
+        long pick = Math.abs(factories.get(f-1) - robot.get(r-1)) + solve(r - 1, f - 1, robot, factories, dp);
         long not_pick = solve(r, f - 1, robot, factories, dp);
 
         return dp[r][f] = Math.min(pick, not_pick);
@@ -29,8 +29,8 @@ class Solution {
             return ans;
         }
 
-        long[][] dp = new long[rSize][fSize];
+        long[][] dp = new long[rSize + 1][fSize + 1];
         for(long[] row : dp) Arrays.fill(row, -1);
-        return solve(rSize-1, fSize-1, robot, factories, dp);
+        return solve(rSize, fSize, robot, factories, dp);
     }
 }
