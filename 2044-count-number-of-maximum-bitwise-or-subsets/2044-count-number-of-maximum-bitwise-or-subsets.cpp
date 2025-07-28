433525aa870f3d1abdc1e8c02fe0f8dd9db73c6d
Akshit Mittal <116814372+AkshitMital@users.noreply.github.com>
2025-07-29 02:20:42 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,20 +1,56 @@
 class Solution {
 private:
-    void solve(vector<int>& nums, int ind, unordered_map<int,int>& mpp, int& maxi, int x){
-        if(ind < 0) return;
+    int solve(vector<int>& nums, int ind, int x, vector<vector<int>>& dp, int maxi){
+        if(ind < 0) return (x == maxi) ? 1 : 0;
+
+        if(dp[ind][x] != -1) return dp[ind][x];
 
         int tempxor = x | nums[ind];
-        maxi = max(maxi, tempxor);
-        mpp[tempxor]++;
+        
+        int exclude = solve(nums, ind-1, x, dp, maxi);
+        int include = solve(nums, ind-1, tempxor, dp, maxi);
 
-        solve(nums, ind-1, mpp, maxi, x);
-        solve(nums, ind-1, mpp, maxi, tempxor);
+        return dp[ind][x] = include + exclude;
     }
 public:
     int countMaxOrSubsets(vector<int>& nums) {
-        unordered_map<int,int> mpp;
+        int n = nums.size();
+
         int maxi = 0;
-        solve(nums, nums.size()-1, mpp, maxi, 0);
-        return mpp[maxi];
+        for(int num : nums) maxi |= num;
+
+        vector<vector<int>> dp(n, vector<int>(maxi+1, -1));
+
+        return solve(nums, n-1, 0, dp, maxi);
+    }
+};
+
+class Solution {
+private:
+    int solve(vector<int>& nums, int ind, int x, vector<vector<int>>& dp, int maxi){
+        if(ind < 0) return (x == maxi) ? 1 : 0;
+
+        if(dp[ind][x] != -1) return dp[ind][x];
+
+        int tempxor = x | nums[ind];
+        
+        int exclude = solve(nums, ind-1, x, dp, maxi);
+        int include = solve(nums, ind-1, tempxor, dp, maxi);
+
+        return dp[ind][x] = include + exclude;
+    }
+public:
+    int countMaxOrSubsets(vector<int>& nums) {
+        int n = nums.size();
+
+        int maxi = 0;
+        for(int num : nums) maxi |= num;
+
+        vector<vector<int>> dp(n, vector<int>(maxi+1, 0));
+        dp[]
+
+        for(int i)
+
+        return dp[n-1][maxi];
     }
 };
